c0d61f708f376eed57ed26595658f4e212e1519a
MANUELY DA SILVA VIANA <25162431@edu119.local>
2026-03-06 16:49:43 -0300

    primeiro commit

---
+++ src/routes/usuario.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as usuarioController from "../controllers/usuario.controller.js"
+
+const router = Router();
+
+router.post("/login", usuarioController.login);
+
+router.get("/", usuarioController.listar);
+router.get("/:id", usuarioController.buscarPorId);
+router.post("/", usuarioController.criar);
+
+export default router;
